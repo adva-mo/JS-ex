c687a13dbdf5b601bdaa1b71057fc441fb58f0a1
adva-mo <advamozes@gmail.com>
2022-09-18 14:58:55 +0300

    1.1

---
+++ 1.1.js
@@ -0,0 +1,15 @@
+let my_string = 'this is a string';
+let my_number = 9;
+let my_object = {prop1:'prop1'};
+let my_bolean = false;
+let my_null = null;
+let my_undefiened = undefined;
+let my_symbol = Symbol('x');
+
+console.log(typeof(my_string));
+console.log(typeof(my_number));
+console.log(typeof(my_object));
+console.log(typeof(my_bolean));
+console.log(typeof(my_null));
+console.log(typeof(my_undefiened));
+console.log(typeof(my_symbol));
