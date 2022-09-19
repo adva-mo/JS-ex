c394550ff2f60d19b06af3484e67b20c8ce3f1d8
adva-mo <advamozes@gmail.com>
2022-09-19 12:24:13 +0300

    ex 4.2

--- 4.1.js
+++ 4.1.js
@@ -1,7 +1,6 @@
 // 'use strict';
 
 const prompt = require('prompt-sync')({sigint: true});
-    // let guess = prompt('What is your guess?:');
 let answer = parseInt(prompt('how many siblings do you have?'));
 if (answer === 1 ){
     console.log('1 sibling!');
