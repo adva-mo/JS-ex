8dfff542a41fbfce01943d8ed2d378dd4043a6e6
adva-mo <advamozes@gmail.com>
2022-09-20 11:08:25 +0300

    ex 5.3

---
+++ 5.3.js
@@ -0,0 +1,37 @@
+// --------------if/else
+
+function passwordValidation(password){
+    if (password.length>7){
+        return 'strong'
+    } else {
+        return 'weak'
+    }
+}
+
+// -------------ternanry 
+
+function passwordValidation2(password){
+    let res = (password.length>7)?'strong':'weak';
+    return res  
+}
+
+
+//  ----------&&
+
+function passwordValidation3(password){
+    let res = password.length>7&&'strong';
+    return res
+}
+
+
+//-------- advanced validation
+
+function advanced(password){
+    let res = password.length>7?password == password.toLowerCase?'strong':'very strong':'weak';
+    return res
+}
+
+
+// console.log(advanced('AIUbbbbbbb'));
+
+
