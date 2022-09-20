2251eb8afccb5192c402c93af67b14674a5c79d3
adva-mo <advamozes@gmail.com>
2022-09-20 11:19:47 +0300

    ex 5.4

---
+++ 5.4.js
@@ -0,0 +1,34 @@
+function func(color) {
+  switch (color) {
+    case "yellow":
+      return "light color";
+      break
+    case "pink":
+      return "light color";
+      break
+    case "orange":
+      return "light color";
+      break
+    case "blue":
+      return "dark-color";
+      break
+    case "purple":
+      return "dark-color";
+      break
+    case "brown":
+      return "dark-color";
+      break
+    default:
+      return "Unknown color";
+  }
+}
+
+console.log(func('blue'));
+
+// Create a function that receives one argument, a string color.
+// Create a switch statement that:
+// 1. If the color is yellow, pink, or orange return from the
+// function “light color”.
+// 2. If the color is a blue, purple, or brown return from the
+// function “dark color”.
+// 3. If the color is none of the above return “Unknown color”
