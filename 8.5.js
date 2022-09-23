b324ff4890c0323d14f7f6ec4e62568eb8423b38
adva-mo <advamozes@gmail.com>
2022-09-23 15:39:24 +0300

    ex 8.5

--- 8.5.js
+++ 8.5.js
@@ -1,3 +1,15 @@
-function reverseObject(){
-    
+function reverseObject(object){
+    let values = Object.keys(object);
+    let keys = Object.values(object);
+    var result = {};
+    keys.forEach((key, i) => result[key] = values[i]);
+    return result;
 }
+
+myobject= {
+    1:'a',
+    2:'b',
+    3:'c'
+}
+
+console.log(reverseObject(myobject));
