e52443f287526255984bcf1b90902fe386e7992a
adva-mo <advamozes@gmail.com>
2022-09-23 15:21:35 +0300

    ex 7.2 done

--- 7.2.js
+++ 7.2.js
@@ -1,16 +1,9 @@
-function checkOccourence(string){
-    let counter = 0;
-    for (l of string){
-        if(l==='y'){
-            if(l++=='o'){
-                if(l+2=='u'){
-                     
-                }
-            }
-        }
-        counter+=1
-    } 
+function checkOccourence(str) {
+  let occourence = str.lastIndexOf('y');
+  return occourence
+
 }
 
 
 const string = "The more you know, the more you know that you don't know";
+console.log(checkOccourence(string));
