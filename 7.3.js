e9980c92fc4c73e70db0964e522dcbc581e4f870
adva-mo <advamozes@gmail.com>
2022-09-26 16:01:05 +0300

    ex 7.3

---
+++ 7.3.js
@@ -0,0 +1,12 @@
+
+// Use slice create a new array that will remove all the
+// elements from the array except “Orange” and “Lemon”
+
+
+
+const fruits = ["Banana", "Orange", "Lemon",
+"Apple", "Mango"];
+
+let newArray = fruits.slice(1,3);
+console.log(newArray);
+
