61ec3750fc35662c31d627267516cb5ca37664ce
adva-mo <advamozes@gmail.com>
2022-09-29 15:16:32 +0300

    ex 11.3 not done

---
+++ 11.3.js
@@ -0,0 +1,86 @@
+const data = [
+  {
+    name: "John",
+    birthday: "1-1-1995",
+    favoriteFoods: {
+      meats: ["hamburgers", "sausages"],
+      fish: ["salmon", "pike"],
+    },
+  },
+  {
+    name: "Mark",
+    birthday: "10-5-1980",
+    favoriteFoods: {
+      meats: ["hamburgers", "steak", "lamb"],
+      fish: ["tuna", "salmon", "barracuda"],
+    },
+  },
+  {
+    name: "Mary",
+    birthday: "1-10-1977",
+    favoriteFoods: {
+      meats: ["ham", "chicken"],
+      fish: ["pike"],
+    },
+  },
+  {
+    name: "Thomas",
+    birthday: "1-10-1990",
+    favoriteFoods: {
+      meats: ["bird", "rooster"],
+      fish: ["salmon"],
+    },
+  },
+  {
+    name: "Mary",
+    birthday: "1-10-1977",
+    favoriteFoods: {
+      meats: ["hamburgers", "lamb"],
+      fish: ["anchovies", "tuna"],
+    },
+  },
+];
+
+// 1. Create a function that returns all the names from the array.
+
+function getNames(arr) {
+  let res = arr.map(function (person) {
+    return person.name;
+  });
+  return res;
+}
+
+// 2. Create a function that returns all the objects that are born
+// before 1990.
+
+function getDates(arr) {
+  let res = [];
+  arr.forEach(function (person) {
+    let year = person.birthday.split("-");
+    year = parseInt(year[2]);
+    if (year < 1990) {
+      res.push(person);
+    }
+  });
+  return res;
+}
+
+// 3. Create a function that returns an object of all the different
+// foods from all the objects as the key and the number of
+// times that food is present in all the objects as the value.
+
+function getFood(arr) {
+  let res = {};
+  arr.forEach(function (p) {
+    p.favoriteFoods.meats.forEach(function (i) {
+      if (!res.hasOwnProperty(i)) {
+        res.i = 1;
+      }
+    });
+  });
+  return res;
+}
+
+// let x = data[0];
+// x = x.favoriteFoods; //object
+// x = x.meats;
