fabb011c0ec516612517aa7f5d3d5322e92086da
adva-mo <advamozes@gmail.com>
2022-09-29 15:22:18 +0300

    ex 12.1

---
+++ 12.1.js
@@ -0,0 +1,31 @@
+// Write a JavaScript function that receives the following library
+// object as an input and displays the books that can be read (the
+// reading status is true).
+// Log the book name, author name, and reading status.
+
+const library = [
+  {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    readingStatus: true,
+  },
+  {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+    readingStatus: true,
+  },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false,
+  },
+];
+
+function displayBooks(arr) {
+  arr.forEach(function (b) {
+    if (b.readingStatus == true) {
+      console.log(`${b.title} by ${b.author} is available for reading!`);
+    }
+  });
+}
+displayBooks(library);
