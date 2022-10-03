5cae4e0f1adedb904e1f89271688b4f482e4f500
adva-mo <advamozes@gmail.com>
2022-10-03 18:11:15 +0300

    13.1 done

--- 13.1.js
+++ 13.1.js
@@ -3,11 +3,9 @@ const numbers = [1, -5, 666, 2, 400, 11];
 // 1. Sort the array of numbers from descending to ascending
 // order.
 
-const sorted = numbers.sort();
-// console.log(sorted);
+console.log(numbers.sort((a, b) => a - b));
+
 // 2. Sort the array of numbers from ascending to descending
 // order.
-numbers.sort(function (a, b) {
-  return b - a;
-});
+numbers.sort((a, b) => b - a);
 console.log(numbers);
