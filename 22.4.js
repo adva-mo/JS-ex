b4f31d277346c10afaf3959ba0ac97784791a193
adva-mo <advamozes@gmail.com>
2022-10-19 09:00:14 +0300

    22.4.modified

--- 22.4.js
+++ 22.4.js
@@ -1,25 +1,25 @@
 const player1 = document.querySelector("#player1-race");
 const player2 = document.querySelector("#player2-race");
-let player1C = player1.firstElementChild;
-let player2C = player2.firstElementChild;
+let p1 = player1.firstElementChild;
+let p2 = player2.firstElementChild;
 
 window.addEventListener("keyup", (e) => {
   const key = e.key;
   if (key === "a") {
-    if (player1C.nextElementSibling !== null) {
-      player1C.classList.remove("active");
-      player1C = player1C.nextElementSibling;
-      player1C.classList.add("active");
+    if (p1.nextElementSibling !== null) {
+      p1.classList.remove("active");
+      p1 = p1.nextElementSibling;
+      p1.classList.add("active");
     } else {
       alert("Player 1 Won");
     }
   }
 
   if (key === "d") {
-    if (player2C.nextElementSibling !== null) {
-      player2C.classList.remove("active");
-      player2C = player2C.nextElementSibling;
-      player2C.classList.add("active");
+    if (p2.nextElementSibling !== null) {
+      p2.classList.remove("active");
+      p2 = p2.nextElementSibling;
+      p2.classList.add("active");
     } else {
       alert("Player 2 Won");
     }
