605192d4bed41eb9fd6ad1f33e35d1e609d88353
adva-mo <advamozes@gmail.com>
2022-10-26 11:56:32 +0300

    ex 29.1

---
+++ 29.1.js
@@ -0,0 +1,48 @@
+const getIDs = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([532, 543, 753, 1, 5]);
+    }, 1500);
+  });
+
+const getRecipe = (recipeID) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      (ID) => {
+        const recipe = {
+          title: "Fresh tomato pasta",
+          publisher: "Pinchas Hodadad",
+        };
+        resolve(`${ID}: ${recipe.title}`);
+      },
+      1500,
+      recipeID
+    );
+  });
+};
+
+// getIDs()
+//   .then((IDs) => {
+//     console.log(IDs);
+//     return getRecipe(IDs[2]);
+//   })
+//   .then((recipe) => {
+//     console.log(recipe);
+//   })
+//   .catch((error) => {
+//     console.log("It is an error!");
+//   });
+
+async function getid2() {
+  try {
+    const reciepeID = await getIDs();
+    const reciepe = await getRecipe(reciepeID);
+    console.log(reciepe);
+  } catch {
+    console.log("error");
+  }
+}
+getid2();
+
+//     Your task is to convert the consumed promise not to use .then
+// and .catch but to use the modern way using async and await.
